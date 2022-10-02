11b4847c4652699050a16cf651bde378469b7c8b
grumpyaddons <grumpyaddons@users.noreply.github.com>
2022-10-01 21:39:30 -0500

    Minimal interface options panel in case people get lost

---
+++ MacroManager/Layout/InterfaceOptions.lua
@@ -0,0 +1,32 @@
+local _, Private = ...;
+
+-- Contain accessing undefined variables to one place to remove linter warnings.
+local InterfaceOptions_AddCategory, CreateFrame, CloseWindows = InterfaceOptions_AddCategory, CreateFrame, CloseWindows;
+
+
+local panel = CreateFrame("Frame")
+panel.name = "MacroManager"
+InterfaceOptions_AddCategory(panel)
+
+local title = panel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge");
+title:SetPoint("TOPLEFT", 10, -15);
+title:SetText("MacroManager");
+
+local button = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
+button:SetText("Open MacroManager");
+button:SetPoint("TOPLEFT", title, 5, -25);
+button:SetSize(150, 28)
+button:SetScript("OnClick", function()
+   while CloseWindows() do end;
+   Private.main.ToggleWindow();
+end)
+
+local instructions = panel:CreateFontString("ARTWORK", nil, "GameFontWhite");
+instructions:SetJustifyH("LEFT")
+instructions:SetJustifyV("TOP")
+instructions:SetPoint("TOPLEFT", button, 5, -35);
+instructions:SetText([[
+Type `/mm` or `/macromanager` to open the MacroManager UI.
+
+Built by Grrumpy <Coldblooded> - Faerlina (US)
+]]);
--- default.ps1
+++ default.ps1
@@ -1,14 +1,17 @@
+# Usage: psake default.ps1 Package
+$macroManagerFolderPath = 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\MacroManager';
+$macroManagerDataFolderPath = 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\MacroManagerData';
+
 Task Package {
     Compress-Archive -Path MacroManager,MacroManagerData -DestinationPath package.zip
 }
 
 Task PublishToLocalWoW {
-    $macroManagerFolderPath = 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\MacroManager';
+    
     if (Test-Path $macroManagerFolderPath) {
         Remove-Item $macroManagerFolderPath -Recurse -Force
     }
 
-    $macroManagerDataFolderPath = 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns\MacroManagerData';
     if (Test-Path $macroManagerDataFolderPath) {
         Remove-Item $macroManagerDataFolderPath -Recurse -Force
     }
@@ -16,3 +19,6 @@ Task PublishToLocalWoW {
     Copy-Item '.\MacroManager' 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns' -Recurse
     Copy-Item '.\MacroManagerData' 'C:\Program Files (x86)\World of Warcraft\_classic_\Interface\AddOns' -Recurse
 }
+
+# Dev symlink to local WoW install
+# cmd /c mklink /d $macroManagerFolderPath "./MacroManager"
